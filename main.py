f41457b59f5004ecd40061ea5b0b927206034c18
ajrollerson <alistair.jrollerson@hotmail.co.uk>
2026-02-13 20:54:28 +0700

    Implemented a generate report feature and added additional books.

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 from stats import word_count
 from stats import character_count
+from stats import sorted_char_dict
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
+def generate_report(text):
+    print("============ BOOKBOT ============")
+    print(f"Analysing book found at {sys.argv[1]}")
+    print("----------- Word Count ----------")
+    book = get_book_text(sys.argv[1])
+    total_words = word_count(book)
+    print(f"Found {total_words} total words")
+    print("----------- Character Count ----------")
+    character_count_dict = character_count(book)
+    sorted_chars = sorted_char_dict(character_count_dict)
+    for entry in sorted_chars:
+        print(f"{entry['char']}: {entry['num']}")
+    print("============= END ===============")
+
 def main():
-    text = get_book_text("books/frankenstein.txt")
-    print(f"Found {word_count(text)} total words")
-    print(character_count(text))
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    text = get_book_text(sys.argv[1])
+    generate_report(text)
+    sys.exit(0)
+
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -11,3 +11,16 @@ def character_count(text):
         else:
             character_dict[c] += 1
     return character_dict
+
+    
+def sorted_char_dict(character_count):
+    char_list = []
+    for character, count in character_count.items():
+        if character.isalpha():
+            char_list.append({"char": character, "num": count})
+
+    def sort_on(dict):
+        return dict["num"]
+    
+    return sorted(char_list, reverse=True, key=lambda d: d['num'])
+
